f88bf1f040c442c37fb1397e0c995e39da0feef4
vinay <vinaythativati@gmail.com>
2026-09-20 03:25:47 +0530

    d

--- 04-databases/bootstrap.sh
+++ 04-databases/bootstrap.sh
@@ -1,8 +1,8 @@
 #!bin/bash
-component =$1
+component=$1
 dnf install ansible -y
 mkdir -p /var/log/roboshop/
-chown -R ec2-user: ec2-user /var/log/roboshop
+chown -R ec2-user:ec2-user /var/log/roboshop
 chmod -R 755 /var/log/roboshop
 touch /var/log/roboshop/ansible.log
 
