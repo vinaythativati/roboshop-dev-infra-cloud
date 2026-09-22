dcf33f04d6a6d78249a26eb32891bff621067c54
vinay <vinaythativati@gmail.com>
2026-09-22 13:36:46 +0530

    fd

--- 04-databases/parameter.tf
+++ 04-databases/parameter.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "mysql" {
   name  = "${var.project_name}-${var.env_name}-msql_root_password"
   type  = "String"
-  value =  var.mysql_root_password
+  value =  var.mysql_root_password 
    overwrite = true
 }
