f3c810e1e4491860bdbe0f96ca80aa9eebee1d9a
vinay <vinaythativati@gmail.com>
2026-09-26 19:00:20 +0530

    main

--- 04-databases/main.tf
+++ 04-databases/main.tf
@@ -115,6 +115,7 @@ resource "aws_instance" "mysql" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.mysql_sg_id]
   subnet_id = local.database_subnet_sg_id 
+  iam_instance_profile = aws_iam_instance_profile.mysql.name
 
   tags = merge(var.ec2_tag,
   {  
